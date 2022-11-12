7984762ac1c69f0d7e00c30b98f7895150a1749b
Anton <levantonio@mail.ru>
2022-11-13 02:52:17 +0300

    **

--- homework03/Program.cs
+++ homework03/Program.cs
@@ -5,12 +5,11 @@ int num_b = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число 3");
 int num_c = Convert.ToInt32(Console.ReadLine());
 
-
 int max = num_a;
 
-if (num_a > max) max = num_a;
-if (num_b > max) max = num_b;
-if (num_c > max) max = num_c;
-
+if (num_a > max)
+    max = num_a;
+if (num_b > max)
+    max = num_b;
 
 Console.Write($"max = {max}");
--- homework04/Program.cs
+++ homework04/Program.cs
@@ -1,6 +1,7 @@
 ﻿Console.WriteLine("Введите целое число! ");
 int count = Convert.ToInt32(Console.ReadLine());
- for( int i = 0; i < count+1; i++ )
+for (int i = 0; i < count + 1; i++)
 {
-    if(i % 2==0) Console.Write(i);
+    if (i % 2 == 0)
+        Console.Write(i);
 }
