a22dfb7dad0821baa351c56fa55ee3c5416355df
Anton <levantonio@mail.ru>
2022-11-12 16:57:10 +0300

    homework 01, 02

---
+++ homework01/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число 'a'");
+int num_a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 'b");
+int num_b = Convert.ToInt32(Console.ReadLine());
+if (num_a > num_b)
+{
+    Console.WriteLine($"max = {num_a}");
+}
+if (num_b > num_a)
+{
+    Console.WriteLine($"max = {num_b}");
+}
---
+++ homework02/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите целое число");
+int num_a = Convert.ToInt32(Console.ReadLine());
+if (num_a % 2 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
